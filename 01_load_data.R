8bc80a103ddde438daf10dd1fb1dd28d3a10882b
lqasemzadeh <lqasemzadeh@gmail.com>
2026-04-03 16:19:14 +0200

    explore dataset and filter out non-task interactions

--- 01_load_data.R
+++ 01_load_data.R
@@ -4,3 +4,14 @@ head(data)
 str(data)
 summary(data)
 names(data)
+
+# Explore modes and actions
+unique(data$mode)
+unique(data$action)
+table(data$action)
+
+table(data$action[data$mode == "Information"])
+# remove non-task mode
+data <- subset(data, mode != "Information")
+
+table(data$mode)
